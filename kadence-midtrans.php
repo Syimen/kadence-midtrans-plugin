72da15598cd2ed973f54e33778c3d02abca3c5d5
Syimen <Syimen@users.noreply.github.com>
2025-04-03 16:30:20 +0800

    Update kadence-midtrans.php

---
+++ kadence-midtrans.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Kadence Midtrans Gateway
+Description: Integrasi pembayaran Midtrans untuk Advanced Forms Kadence.
+Version: 1.0
+Author: Syimen
+*/
+
+defined('ABSPATH') or die('No direct access!');
+
+// Load file pendukung
+require_once __DIR__ . '/includes/class-admin.php';
+require_once __DIR__ . '/includes/class-midtrans.php';
